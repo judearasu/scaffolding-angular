39b2a73e3c79472fe0693765a658174f828e1755
Eduardo Gonzalez <eduardo_gonzalez@homedepot.com>
2016-03-16 11:25:53 -0400

    Add terminal image

--- index.js
+++ index.js
@@ -14,31 +14,28 @@ var imaging = require('imaging');
 
 function scaffoldingAngular(template, path) {
 
-
-    imaging.draw('./images/i.jpg', function (resp, status) {
-        /*
-          if status == 'success', resp is the image charater string.
-          conols.log(resp) to render the image in your terminal
-          else if if status == 'fail', resp is the error message
-        */
+    // var command = "imaging ./images/thd_logo.png -l 20 -w 50 -c circle";
+    // console.log(command);
+    //
+    imaging.draw('./images/thd_logo.png', {width: 50}, function (resp, status) {
         console.log(status);
         console.log(resp);
     });
 
-    inquirer.prompt([{
-        type: 'input',
-        name: 'project_name',
-        message: 'Hola Deepak:',
-        default: 'Gracias bro!!!!'
-    }, ], function(project_answers) {
-        try {
-            fs.copySync('./templates/components/src', './tmp');
-            console.log("success!");
-        } catch (err) {
-            console.error(err);
-        }
-    });
 
+    // inquirer.prompt([{
+    //     type: 'input',
+    //     name: 'project_name',
+    //     message: 'Hola Deepak:',
+    //     default: 'Gracias bro!!!!'
+    // }, ], function(project_answers) {
+    //     try {
+    //         fs.copySync('./templates/components/src', './tmp');
+    //         console.log("success!");
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // });
     return true;
 }
 
