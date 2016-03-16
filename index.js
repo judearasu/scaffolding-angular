fcb980f961e38d2bc5cdebe5d7a0b2be56f0be29
Eduardo Gonzalez <eduardo_gonzalez@homedepot.com>
2016-03-16 16:01:15 -0400

    Add template test name and handle correct path project

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ var welcomeImg = require('./images/welcomeImg.js');
 /**
  * Main function to copy template files into the main project
  * @param {object} template
- * @param {object} path
+ * @param {object} pathScaffolding
  */
 
-function scaffoldingAngular(template, path) {
+module.exports = function scaffoldingAngular(template, pathScaffolding) {
     console.log(welcomeImg);
+    var appDir = path.resolve(__dirname + '../../../');
 
     inquirer.prompt([{
         type: 'input',
@@ -24,17 +25,16 @@ function scaffoldingAngular(template, path) {
         default: 'TestComponents'
     }, ], function(project_answers) {
         try {
-            fs.copySync(__dirname + '/node_modules/scaffolding-angular/templates/components/src', __dirname +'/source/app/components/test');
+            fs.copySync(__dirname + pathScaffolding, appDir + '/source/app/components/'+template);
             chalk.green("Success! - Component Created");
         } catch (err) {
             console.log(err);
             chalk.red("Error creating the Component");
         }
     });
-}
+};
 
 /**
 * Call main function for testing
 */
 // scaffoldingAngular('testComponent', '/source/app/components');
-module.exports = scaffoldingAngular;
