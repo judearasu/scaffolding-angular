cad7d3f3bc8aa28eb218beff3fd624a492ffbaa8
Thillai Arasu Narayanan <thillaiarasu_narayanan@homedepot.com>
2016-03-16 15:04:27 -0400

    ability to create folders

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * Module Scaffolding Angular
  * MyProDeskUI - Home Depot
  */
-var inquirer = require('inquirer');
-var fs = require('fs-extra');
-var imaging = require('imaging');
+//'use strict';
+var gulp = require('gulp'),
+    inquirer = require('inquirer'),
+    fs = require('fs-extra'),
+    imaging = require('imaging');
 
 /**
  * Main function to copy template files into the main project
@@ -15,36 +17,37 @@ var imaging = require('imaging');
 function scaffoldingAngular(template, path) {
 
 
-    imaging.draw('./images/i.jpg', function (resp, status) {
-        /*
-          if status == 'success', resp is the image charater string.
-          conols.log(resp) to render the image in your terminal
-          else if if status == 'fail', resp is the error message
-        */
-        console.log(status);
-        console.log(resp);
-    });
+	imaging.draw('./images/i.jpg', function(resp, status) {
+		console.log(status);
+		console.log(resp);
+	});
 
-    inquirer.prompt([{
-        type: 'input',
-        name: 'project_name',
-        message: 'Hola Deepak:',
-        default: 'Gracias bro!!!!'
-    }, ], function(project_answers) {
-        try {
-            fs.copySync('./templates/components/src', './tmp');
-            console.log("success!");
-        } catch (err) {
-            console.error(err);
-        }
-    });
+	inquirer.prompt([{
+		type: 'input',
+		name: 'module_name',
+		message: 'Type the name of the module',
+		default: 'projectName'
+	}, {
+		type: 'input',
+		name: 'component_name',
+		message: 'Type the name of the component you want to create',
+		default: 'componentOne'
+	}], function(project_answers) {
+		userDefaults = {
+			project_module_name: project_answers.module_name,
+			project_component_name: project_answers.component_name
+		};
 
-    return true;
+    var componentName = project_answers.component_name;
+
+	});
+
+
 }
 
 /**
-* Call main function for testing
-*/
+ * Call main function for testing
+ */
 scaffoldingAngular();
 
 module.exports = scaffoldingAngular;
---
+++ test.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var assert = require('assert');
+var gutil = require('gulp-util');
+var filter = require('./');
+
+describe('Scaffold Angular', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
