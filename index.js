bdd13d7171abb42d5fb7581ee3a904ee3ac2577d
Eduardo Gonzalez <eduardo_gonzalez@homedepot.com>
2016-03-17 15:02:14 -0400

    Add nyancat and clean console

--- index.js
+++ index.js
@@ -7,29 +7,38 @@ var fs = require('fs-extra');
 var path = require("path");
 var chalk = require('chalk');
 var welcomeImg = require('./images/welcomeImg.js');
+var Nyancat = require('nyansole');
+var clear = require("cli-clear");
 
 /**
  * Main function to copy template files into the main project
- * @param {object} template
- * @param {object} pathScaffolding
+ * @param {object} appDir
  */
 
 module.exports = function scaffoldingAngular(appDir) {
-    console.log(welcomeImg);
-    appDir = appDir || path.resolve(__dirname + '../../../');
 
-    inquirer.prompt([{
-        type: 'input',
-        name: 'componentName',
-        message: 'Components Name:',
-        default: 'TestComponents'
-    }, ], function(project_answers) {
-        try {
-            fs.copySync(__dirname + pathScaffolding, appDir + '/source/app/components/'+template);
-            console.log("\n"+chalk.green("Success! - Component Created"));
-        } catch (err) {
-            console.log(err);
-            console.log("\n"+chalk.red("Error creating the Component"));
-        }
-    });
+    clear();
+    appDir = appDir || path.resolve(__dirname + '../../../');
+    var nyancat = (new Nyancat()).start();
+
+    setTimeout(function () {
+        console.log(welcomeImg);
+
+        inquirer.prompt([{
+            type: 'input',
+            name: 'componentName',
+            message: 'Components Name:',
+            default: 'TestComponents'
+        }, ], function(project_answers) {
+            try {
+                fs.copySync(__dirname + '/templates/components', appDir + '/source/app/components/');
+                console.log("\n"+chalk.green("Success! - Component Created"));
+            } catch (err) {
+                console.log("\n"+chalk.red("Error creating the Component"));
+            }
+        });
+
+        nyancat.end();
+    }, 1400);
+
 };
