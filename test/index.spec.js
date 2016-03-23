6ff6e64fa4dc7d3b345b76f9f0fd78b1002ca38c
Thillai Arasu Narayanan <thillaiarasu_narayanan@homedepot.com>
2016-03-23 16:56:01 -0400

    base version of index specs

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,26 +1,9 @@
-var angularScaffold = require('../index.js');
-
-describe.only('index', function() {
-  // console.log(angularScaffold);
-
-  var promptsTerminal = angularScaffold.promptsTerminal();
-  console.log(promptsTerminal);
-
-  // var angularScaffold = {
-  //     promptsTerminal: promptsTerminal,
-  //     promptsTerminal: promptsTerminal,
-  //     promptUser: promptUser
-  // };
-
-  it('should have a valid function with name promptsTerminal', function(done) {
-    console.log(promptsTerminal);
-    promptsTerminal.should.exist();
-    done();
-  });
-
-  it('should accept a single parameter ', function(done) {
-    promptsTerminal.should.have.length(1);
-    done();
-    // body...
-  });
+describe('angularScaffold', function() {
+    //var scaffoldPrompt = require('../index.js').promptsTerminal();
+    var angularScaffold = require('../index.js');
+    it('should return object', function() {
+        angularScaffold.should.be.an.instanceOf(Object);
+    });
+    // Check the object length
+    
 });
