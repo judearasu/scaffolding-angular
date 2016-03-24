a1e2d7a4131f49ecf0c558e8cc1025eeecef8aa0
Deepak Srinivasan <deepak_srinivasan_selvarajan1@homedepot.com>
2016-03-24 11:34:20 -0400

    test cases for index.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,20 @@
-describe('angularScaffold', function() {
+describe.only('angularScaffold', function() {
     //var scaffoldPrompt = require('../index.js').promptsTerminal();
     var angularScaffold = require('../index.js');
-    it('should return object', function() {
-        angularScaffold.should.be.an.instanceOf(Object);
+    // var appDir = __dirname;
+    var promptUser = angularScaffold.promptUser;
+    var promptsTerminal = angularScaffold.promptsTerminal();
+    var appStart = angularScaffold.appStart;
+    it('should exist function named promptUser', function() {
+        promptUser.should.be.Function();
     });
-    // Check the object length
-    
+
+    it('should exist function named appStart', function() {
+        appStart.should.be.Function();
+    });
+
+    it('should be an array of length 3', function(done) {
+        promptsTerminal.should.be.instanceof(Array).and.have.lengthOf(3);
+        done();
+    });
 });
