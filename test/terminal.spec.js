60a829dd2dcd4f8fc291c2c8c7d33c485c1aee53
Eduardo Gonzalez <eduardo_gonzalez@homedepot.com>
2016-03-28 16:08:18 -0400

    Add new test cases

--- test/terminal.spec.js
+++ test/terminal.spec.js
@@ -1,3 +1,5 @@
+var constants = require('../constants.js');
+
 describe('Nyansole', function() {
     var Nyancat = require('nyansole');
 
@@ -18,6 +20,20 @@ describe('Welcome Image', function() {
 
     it('should welcome image be of length 10', function(done) {
         welcomeImg.should.have.length(379);
+        done();
+    });
+});
+
+describe('Option List Terminal Prompt', function() {
+    var scaffoldPrompt = require('../util.js').optionListTerminal();
+
+    it('should Component input needs to have this properties', function(done) {
+        var modulePrompt = scaffoldPrompt[0];
+        modulePrompt.type.should.equal('list');
+        modulePrompt.name.should.equal(constants.optionList.NAME);
+        modulePrompt.message.should.equal(constants.optionList.MESSAGE);
+        modulePrompt.choices.should.equal(constants.optionList.CHOICES);
+        modulePrompt.default.should.equal(constants.optionList.DEFAULT);
         done();
     });
 });
@@ -54,6 +70,32 @@ describe('Components Terminal Prompt', function() {
         modulePrompt.type.should.equal('confirm');
         modulePrompt.name.should.equal('spec');
         modulePrompt.message.should.equal('Do you want to include unit testing?');
+        done();
+    });
+});
+
+describe('ngService Terminal Prompt', function() {
+    var scaffoldPrompt = require('../util.js').ngServiceTerminal();
+
+    it('should ngService input needs to have this properties', function(done) {
+        var modulePrompt = scaffoldPrompt[0];
+        modulePrompt.type.should.equal('input');
+        modulePrompt.name.should.equal(constants.ngService.FOLDER_NAME);
+        modulePrompt.message.should.equal(constants.ngService.FOLDER_MESSAGE);
+        modulePrompt.default.should.equal(constants.ngService.FOLDER_DEFAULT);
+        done();
+    });
+});
+
+describe('mockServiceTerminal Terminal Prompt', function() {
+    var scaffoldPrompt = require('../util.js').mockServiceTerminal();
+
+    it('should mockServiceTerminal input needs to have this properties', function(done) {
+        var modulePrompt = scaffoldPrompt[0];
+        modulePrompt.type.should.equal('input');
+        modulePrompt.name.should.equal(constants.mockService.FOLDER_NAME);
+        modulePrompt.message.should.equal(constants.mockService.FOLDER_MESSAGE);
+        modulePrompt.default.should.equal(constants.mockService.FOLDER_DEFAULT);
         done();
     });
 });
--- test/util.spec.js
+++ test/util.spec.js
@@ -1,16 +1,12 @@
-describe('util', function() {
-    var util = require('../util.js'),
-        getGlobalOptions = util.getGlobalOptions,
-        componentsTerminal = util.componentsTerminal,
-        ngServiceTerminal = ngServiceTerminal,
-        mockServiceTerminal = mockServiceTerminal,
-        appStart = util.appStart;
+var util = require('../util.js'),
+    getGlobalOptions = util.getGlobalOptions,
+    componentsTerminal = util.componentsTerminal,
+    optionListTerminal = util.optionListTerminal,
+    ngServiceTerminal = util.ngServiceTerminal,
+    mockServiceTerminal = util.mockServiceTerminal,
+    appStart = util.appStart;
 
-    it('appstart should be a Function', function(done) {
-        appStart.should.be.Function();
-        done();
-    });
-
+describe('getGlobalOptions function in util.js', function() {
     it('getGlobalOptions should be a Function', function(done) {
         getGlobalOptions.should.be.Function();
         done();
@@ -20,10 +16,59 @@ describe('util', function() {
         Object.keys(getGlobalOptions()).should.have.length(3);
         done();
     });
+});
+
+describe('optionListTerminal function in util.js', function() {
+    it('optionListTerminal should be a Function', function(done) {
+        optionListTerminal.should.be.Function();
+        done();
+    });
+
+    it('optionListTerminal should be an array of length 1', function(done) {
+        optionListTerminal().should.be.instanceof(Array).and.have.lengthOf(1);
+        done();
+    });
+});
+
+describe('componentsTerminal function in util.js', function() {
+    it('componentsTerminal should be a Function', function(done) {
+        componentsTerminal.should.be.Function();
+        done();
+    });
 
     it('componentsTerminal should be an array of length 4', function(done) {
         componentsTerminal().should.be.instanceof(Array).and.have.lengthOf(4);
         done();
     });
+});
 
+describe('ngServiceTerminal function in util.js', function() {
+    it('optionListTerminal should be a Function', function(done) {
+        optionListTerminal.should.be.Function();
+        done();
+    });
+
+    it('ngServiceTerminal should be an array of length 1', function(done) {
+        ngServiceTerminal().should.be.instanceof(Array).and.have.lengthOf(1);
+        done();
+    });
+});
+
+describe('mockServiceTerminal function in util.js', function() {
+    it('mockServiceTerminal should be a Function', function(done) {
+        mockServiceTerminal.should.be.Function();
+        done();
+    });
+
+    it('mockServiceTerminal should be an array of length 1', function(done) {
+        mockServiceTerminal().should.be.instanceof(Array).and.have.lengthOf(1);
+        done();
+    });
+});
+
+describe('appStart function in util.js', function() {
+    it('appstart should be a Function', function(done) {
+        appStart.should.be.Function();
+        done();
+    });
 });
