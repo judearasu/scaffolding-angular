b85aa45ffa68dcb83003c8e0cf49e78eab7ede07
Thillai Arasu Narayanan <thillaiarasu_narayanan@homedepot.com>
2016-03-30 13:46:51 -0400

    version number upgraded

---
+++ templates/javascript/service/template.service.js
@@ -0,0 +1,12 @@
+angular.module('customer-service')
+    .service('CustomerService', CustomerService);
+
+CustomerService.$inject = ['$resource','$http', 'CONFIG'];
+
+function CustomerService($resource, $http, CONFIG) {
+
+
+    var service = this;
+
+    
+}
